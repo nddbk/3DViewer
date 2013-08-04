0dd453d233c3dcebe098efff7b7573aedd5bdc11
dongnd <dongnd@appdev.vn>
2013-08-05 00:06:01 +0700

    First demo.

--- assets/js/templates/panel.js
+++ assets/js/templates/panel.js
@@ -8,11 +8,11 @@ var Template = Template || {}
 
 Template.panel = [
 	'<div class="btns">',
-		'<span class="ctrl panel-item" cmd="zoom-in" title="Wheel Up">Zoom-in</span>',
-		'<span class="ctrl panel-item" cmd="zoom-out" title="Wheel Down">Zoom-out</span>',
-		'<span class="ctrl panel-item" cmd="rotate-top" title="Up Arrow">Rotate Top</span>',
-		'<span class="ctrl panel-item" cmd="rotate-bottom" title="Down Arrow">Rotate Bottom</span>',
-		'<span class="ctrl panel-item" cmd="rotate-left" title="Left Arrow">Rotate Left</span>',
-		'<span class="ctrl panel-item" cmd="rotate-right" title="Right Arrow">Rotate Right</span>',
+		'<button class="panel-item" cmd="zoom-in" title="Wheel Up">Zoom-in</button>',
+		'<button class="panel-item" cmd="zoom-out" title="Wheel Down">Zoom-out</button>',
+		'<button class="panel-item" cmd="rotate-top" title="Up Arrow">Rotate Top</button>',
+		'<button class="panel-item" cmd="rotate-bottom" title="Down Arrow">Rotate Bottom</button>',
+		'<button class="panel-item" cmd="rotate-left" title="Left Arrow">Rotate Left</button>',
+		'<button class="panel-item" cmd="rotate-right" title="Right Arrow">Rotate Right</button>',
 	'</div>'
 ].join('');
